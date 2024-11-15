cb5a19a9b7718a88e06d1dfe29fc4a1e541ffc9a
Phương Nhi <0707213903nhi@gmail.com>
2024-11-16 01:21:50 +0700

    phuong nhi

--- MyApp.py
+++
@@ -1,9 +0,0 @@
-from PyQt5.QtWidgets import QApplication, QMainWindow
-
-app=QApplication([])
-mainwindow=QMainWindow()
-myui=MainWindowEx()
-myui.setupUi(mainwindow)
-myui.setupSignalAndSlot()
-myui.showWindow()
-app.exec()
